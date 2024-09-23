1032783dbf622b5697ce3cae5d1255b56c1ca95d
Wuemeli <wuemeli@gmail.com>
2024-09-23 23:41:24 +0200

    Update Docker

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'wxt';
 
 export default defineConfig({
     manifest: {
-        cors: {
-            origin: '*',
-            methods: ['GET'],
-        },
+        name: 'Link Defender',
+        version: '0.0.1',
+        description: 'A browser extension that helps you to avoid phishing links.',
     }
 });
